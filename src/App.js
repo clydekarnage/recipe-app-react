77fb855646d706022fadcefd7a4d1f6aa390d0ea
clydekarnage <clydeibalez@gmail.com>
2020-05-10 13:46:08 +0400

    fetching the data from the API

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import "./App.css";
 
 import Form from "./components/Form";
 
+const API_KEY = "db254b5cd61744d39a2deebd9c361444";
+
 class App extends Component {
-  getRecipe = (e) => {
+  getRecipe = async (e) => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
+
+    const api_call = await fetch(
+      `https://api.spoonacular.com/recipes/716429/information?apiKey=${API_KEY}&includeNutrition=true.`
+    );
+
+    const data = await api_call.json();
+    console.log(data.extendedIngredients);
+
     console.log(recipeName);
   };
   render() {
